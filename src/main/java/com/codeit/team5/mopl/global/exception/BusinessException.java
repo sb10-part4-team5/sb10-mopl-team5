0bb7aacf9dca21374ee2bb1608967a758f0b8634
plzslp <gyutann1171@gmail.com>
2026-06-20 20:42:04 +0900

    feat: BusinessException 클래스 작성

---
+++ src/main/java/com/codeit/team5/mopl/global/exception/BusinessException.java
@@ -0,0 +1,19 @@
+package com.codeit.team5.mopl.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode, String detailMessage) {
+        super(detailMessage);
+        this.errorCode = errorCode;
+    }
+}
