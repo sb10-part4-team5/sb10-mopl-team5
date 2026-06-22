20a609aabfd08e8769aea6da9e52fe569ee87160
shong9124 <shifty393@gmail.com>
2026-06-22 18:25:24 +0900

    refactor: swagger 문서용 UserApi 수정

--- src/main/java/com/codeit/team5/mopl/user/controller/api/UserApi.java
+++ src/main/java/com/codeit/team5/mopl/user/controller/api/UserApi.java
@@ -2,6 +2,9 @@ package com.codeit.team5.mopl.user.controller.api;
 
 import com.codeit.team5.mopl.user.dto.request.UserRegisterRequest;
 import com.codeit.team5.mopl.user.dto.response.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -10,5 +13,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Tag(name = "사용자 관리", description = "사용자 관련 API")
 public interface UserApi {
 
+    @Operation(summary = "회원가입", description = "새로운 사용자를 등록합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청(입력값 검증 실패)"),
+            @ApiResponse(responseCode = "409", description = "이메일 중복"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
+    })
     ResponseEntity<UserResponse> registerUser(@Valid @RequestBody UserRegisterRequest userRegisterRequest);
 }
