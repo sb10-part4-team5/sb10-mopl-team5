f9b6016fdac38f45de57697316603d276bb99120
shong9124 <shifty393@gmail.com>
2026-06-22 18:07:37 +0900

    feat: 사용자 예외처리 추가

--- src/main/java/com/codeit/team5/mopl/global/exception/ErrorCode.java
+++ src/main/java/com/codeit/team5/mopl/global/exception/ErrorCode.java
@@ -18,6 +18,7 @@ ErrorCode {
     // User
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다."),
+    INVALID_USERNAME(HttpStatus.UNAUTHORIZED, "사용자 이름은 공백일 수 없습니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 올바르지 않습니다."),
     ACCOUNT_LOCKED(HttpStatus.FORBIDDEN, "잠긴 계정입니다."),
 
