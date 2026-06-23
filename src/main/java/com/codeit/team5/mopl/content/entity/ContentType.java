bc1c868fba2c987a0be549b1f473074b3f84df18
plzslp <gyutann1171@gmail.com>
2026-06-23 13:36:43 +0900

    feat: ContentType Json 변환 로직 추가

--- src/main/java/com/codeit/team5/mopl/content/entity/ContentType.java
+++ src/main/java/com/codeit/team5/mopl/content/entity/ContentType.java
@@ -1,5 +1,10 @@
 package com.codeit.team5.mopl.content.entity;
 
+import com.codeit.team5.mopl.content.exception.ContentException;
+import com.codeit.team5.mopl.global.exception.ErrorCode;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ContentType {
     MOVIE("movie"),
     TV_SERIES("tvSeries"),
@@ -9,5 +14,20 @@ public enum ContentType {
 
     ContentType(String response) {
         this.response = response;
+    }
+
+    @JsonValue
+    public String getResponse() {
+        return response;
+    }
+
+    @JsonCreator
+    public static ContentType from(String value) {
+        for (ContentType type : values()) {
+            if (type.response.equals(value)) {
+                return type;
+            }
+        }
+        throw new ContentException(ErrorCode.INVALID_INPUT, "알 수 없는 콘텐츠 타입입니다: " + value);
     }
 }
