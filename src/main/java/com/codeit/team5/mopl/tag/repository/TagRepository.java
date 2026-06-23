889f0deb6f6ed3a7d2e313b5372919c1cc2a27ba
plzslp <gyutann1171@gmail.com>
2026-06-23 18:13:34 +0900

    feat: 콘텐츠, 태그, 콘텐츠 스탯 Repository 추가

---
+++ src/main/java/com/codeit/team5/mopl/tag/repository/TagRepository.java
@@ -0,0 +1,11 @@
+package com.codeit.team5.mopl.tag.repository;
+
+import com.codeit.team5.mopl.tag.entity.Tag;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+
+    Optional<Tag> findByName(String name);
+}
