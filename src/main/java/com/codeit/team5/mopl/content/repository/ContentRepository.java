60b5cf43b65af06c88dcb11a6bcacffcf8812977
plzslp <gyutann1171@gmail.com>
2026-06-24 18:57:17 +0900

    refactor: N+1 문제 해결용 EntityGraph 추가

--- src/main/java/com/codeit/team5/mopl/content/repository/ContentRepository.java
+++ src/main/java/com/codeit/team5/mopl/content/repository/ContentRepository.java
@@ -1,8 +1,13 @@
 package com.codeit.team5.mopl.content.repository;
 
 import com.codeit.team5.mopl.content.entity.Content;
+import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContentRepository extends JpaRepository<Content, UUID> {
+
+    @EntityGraph(attributePaths = {"stats", "contentTags"})
+    Optional<Content> findWithStatsAndTagsById(UUID id);
 }
