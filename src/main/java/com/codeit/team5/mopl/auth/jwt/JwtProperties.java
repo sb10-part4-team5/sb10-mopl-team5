9da0d7524be6ea20dd04a0aacb46126909be421d
shong9124 <shifty393@gmail.com>
2026-06-25 14:54:17 +0900

    feat: JwtToken 생성 세팅
    # Conflicts:
    #	src/main/resources/application.yml
    
    # Conflicts:
    #	src/main/resources/application.yml

---
+++ src/main/java/com/codeit/team5/mopl/auth/jwt/JwtProperties.java
@@ -0,0 +1,11 @@
+package com.codeit.team5.mopl.auth.jwt;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtProperties(
+        String secretKey,
+        long accessTokenExpirationMinutes,
+        long refreshTokenExpirationMinutes
+) {
+}
---
+++ src/main/java/com/codeit/team5/mopl/auth/jwt/JwtTokenizer.java
@@ -0,0 +1,61 @@
+package com.codeit.team5.mopl.auth.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenizer {
+
+    private final JwtProperties jwtProperties;
+
+    public String generateAccessToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date())
+                .setExpiration(getTokenExpiration(jwtProperties.accessTokenExpirationMinutes()))
+                .signWith(getKey())
+                .compact();
+    }
+
+    public String generateRefreshToken(String subject) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(new Date())
+                .setExpiration(getTokenExpiration(jwtProperties.refreshTokenExpirationMinutes()))
+                .signWith(getKey())
+                .compact();
+    }
+
+    public Jws<Claims> getClaims(String jws) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(jws);
+    }
+
+    private Key getKey() {
+        String base64EncodedSecretKey =
+                Encoders.BASE64.encode(jwtProperties.secretKey().getBytes(StandardCharsets.UTF_8));
+
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(base64EncodedSecretKey));
+    }
+
+    private Date getTokenExpiration(long expirationMinutes) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, Math.toIntExact(expirationMinutes));
+        return calendar.getTime();
+    }
+}
