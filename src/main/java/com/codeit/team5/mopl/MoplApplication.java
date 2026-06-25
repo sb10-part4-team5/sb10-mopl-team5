ff9b930af2f218b4362190b19920513f49740539
shong9124 <shifty393@gmail.com>
2026-06-25 17:47:00 +0900

    refactor: 리뷰 반영 - config 클래스로 빼두기

--- src/main/java/com/codeit/team5/mopl/MoplApplication.java
+++ src/main/java/com/codeit/team5/mopl/MoplApplication.java
@@ -1,12 +1,9 @@
 package com.codeit.team5.mopl;
 
-import com.codeit.team5.mopl.auth.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JwtProperties.class)
 public class MoplApplication {
 
     public static void main(String[] args) {
