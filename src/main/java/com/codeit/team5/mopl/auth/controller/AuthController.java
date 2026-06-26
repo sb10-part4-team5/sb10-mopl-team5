08b7f8a3a6ef17debdda9b92f53032550b934f2f
shong9124 <shifty393@gmail.com>
2026-06-26 16:19:40 +0900

    feat: csrf 적용

--- src/main/java/com/codeit/team5/mopl/auth/controller/AuthController.java
+++ src/main/java/com/codeit/team5/mopl/auth/controller/AuthController.java
@@ -12,7 +12,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -66,5 +68,14 @@ public class AuthController implements AuthApi {
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
                 .body(authPayload.jwtResponse());
+    }
+
+    @GetMapping("/csrf-token")
+    public ResponseEntity<Void> csrfToken(CsrfToken csrfToken) {
+        log.info("CSRF token request: GET /api/auth/csrf-token");
+
+        csrfToken.getToken();
+
+        return ResponseEntity.noContent().build();
     }
 }
