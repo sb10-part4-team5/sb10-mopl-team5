3d433796bfd4c13cbcb71258ef79a1d7b572c981
plzslp <gyutann1171@gmail.com>
2026-06-26 11:02:01 +0900

    refactor: ContentNotFoundException details 추가

--- src/main/java/com/codeit/team5/mopl/content/exception/ContentNotFoundException.java
+++ src/main/java/com/codeit/team5/mopl/content/exception/ContentNotFoundException.java
@@ -1,10 +1,12 @@
 package com.codeit.team5.mopl.content.exception;
 
+import java.util.Map;
+import java.util.UUID;
 import org.springframework.http.HttpStatus;
 
 public class ContentNotFoundException extends ContentException {
 
-    public ContentNotFoundException() {
-        super(HttpStatus.NOT_FOUND, "콘텐츠를 찾을 수 없습니다.");
+    public ContentNotFoundException(UUID contentId) {
+        super(HttpStatus.NOT_FOUND, "콘텐츠를 찾을 수 없습니다.", Map.of("contentId", contentId));
     }
 }
