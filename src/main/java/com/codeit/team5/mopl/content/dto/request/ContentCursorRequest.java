95af603141a7c90754b2b674ef6f939816184da9
plzslp <gyutann1171@gmail.com>
2026-06-28 16:53:02 +0900

    refactor: limit 검증에 Positive 추가

--- src/main/java/com/codeit/team5/mopl/content/dto/request/ContentCursorRequest.java
+++ src/main/java/com/codeit/team5/mopl/content/dto/request/ContentCursorRequest.java
@@ -3,6 +3,7 @@ package com.codeit.team5.mopl.content.dto.request;
 import com.codeit.team5.mopl.content.entity.ContentSortByType;
 import com.codeit.team5.mopl.content.entity.ContentType;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 import org.springframework.data.domain.Sort;
 
@@ -18,7 +19,8 @@ public record ContentCursorRequest(
         String idAfter,
 
         @NotNull
-        int limit,
+        @Positive
+        Integer limit,
 
         @NotNull
         Sort.Direction sortDirection,
