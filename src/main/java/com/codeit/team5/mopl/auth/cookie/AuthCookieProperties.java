99c2b5b50a7f611947761231703c497fabe38c8c
shong9124 <shifty393@gmail.com>
2026-06-28 16:16:22 +0900

    fix: 코드래빗 리뷰반영 - null check

--- src/main/java/com/codeit/team5/mopl/auth/cookie/AuthCookieProperties.java
+++ src/main/java/com/codeit/team5/mopl/auth/cookie/AuthCookieProperties.java
@@ -1,5 +1,6 @@
 package com.codeit.team5.mopl.auth.cookie;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
@@ -10,6 +11,7 @@ public record AuthCookieProperties(
 
         boolean secure,
 
+        @NotBlank
         @Pattern(regexp = "Lax|Strict|None")
         String sameSite
 ) {
