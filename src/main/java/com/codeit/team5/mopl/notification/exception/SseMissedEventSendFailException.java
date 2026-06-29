099bb07616841d98d09a74e38f2e449e5d2eb0b8
Seongkyeong Kim <gimmy6707@gmail.com>
2026-06-29 16:37:08 +0900

    Feat: SSE 전달 시 생기는 예외를 대응하기 위한 예외 클래스 작성 - LastEventId가 유효하지 않을 때 (UUID 형식이 아니거나 Blank 일 때) 던지는 InvalidLastEventIdException - 놓친 SseEvent를 전달하는 도중 실패했을 때 대비하는 SseMissedEventSendFailException

---
+++ src/main/java/com/codeit/team5/mopl/notification/exception/SseMissedEventSendFailException.java
@@ -0,0 +1,7 @@
+package com.codeit.team5.mopl.notification.exception;
+
+public class SseMissedEventSendFailExcetion extends RuntimeException {
+  public SseMissedEventSendFailExcetion(String message) {
+    super(message);
+  }
+}
