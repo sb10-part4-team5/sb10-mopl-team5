9334e9256326e3de8031c0423a67883a1d6624df
shong9124 <shifty393@gmail.com>
2026-06-29 15:29:36 +0900

    refactor: MoplUserDetails 추상화 작업

--- src/main/java/com/codeit/team5/mopl/auth/security/details/MoplUserDetailsService.java
+++ src/main/java/com/codeit/team5/mopl/auth/security/details/MoplUserDetailsService.java
@@ -2,13 +2,11 @@ package com.codeit.team5.mopl.auth.security.details;
 
 import com.codeit.team5.mopl.auth.exception.InvalidCredentialsException;
 import com.codeit.team5.mopl.user.entity.User;
-import com.codeit.team5.mopl.user.exception.UserNotFoundException;
 import com.codeit.team5.mopl.user.mapper.UserMapper;
 import com.codeit.team5.mopl.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
