2c55b0c5e89248a1b5862dbfc85ae233952f415b
Seongkyeong Kim <gimmy6707@gmail.com>
2026-07-01 10:56:34 +0900

    Fix: Event 생성 시 파라미터 검증 추가

--- src/main/java/com/codeit/team5/mopl/playlist/entity/event/PlaylistSubscribedEvent.java
+++ src/main/java/com/codeit/team5/mopl/playlist/entity/event/PlaylistSubscribedEvent.java
@@ -1,11 +1,25 @@
 package com.codeit.team5.mopl.playlist.entity.event;
 
+import com.codeit.team5.mopl.notification.exception.InvalidNicknameException;
+import com.codeit.team5.mopl.notification.exception.InvalidReceiverIdException;
+import com.codeit.team5.mopl.playlist.entity.exception.InvalidPlaylistNameException;
 import java.util.UUID;
+import org.springframework.util.StringUtils;
 
 public record PlaylistSubscribedEvent(
     UUID receiverId,
     String subscriberNickname,
     String playlistName
 ) {
-
+    public PlaylistSubscribedEvent{
+        if (receiverId == null){
+            throw new InvalidReceiverIdException();
+        }
+        if(!StringUtils.hasText(subscriberNickname)){
+            throw new InvalidNicknameException();
+        }
+        if(!StringUtils.hasText(playlistName)){
+            throw new InvalidPlaylistNameException();
+        }
+    }
 }
---
+++ src/main/java/com/codeit/team5/mopl/playlist/entity/exception/InvalidPlaylistNameException.java
@@ -0,0 +1,12 @@
+package com.codeit.team5.mopl.playlist.entity.exception;
+
+import com.codeit.team5.mopl.global.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+
+public class InvalidPlaylistNameException extends BusinessException {
+
+    public InvalidPlaylistNameException() {
+
+        super(HttpStatus.BAD_REQUEST, "플레이리스트의 이름이 유효하지 않습니다");
+    }
+}
