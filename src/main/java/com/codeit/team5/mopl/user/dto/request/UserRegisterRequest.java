ed6509768a7f93c0907960d6c4c4fca65108bd09
shong9124 <shifty393@gmail.com>
2026-07-02 13:26:06 +0900

    fix: 비밀번호에 특수문자 허용하도록 수정

--- src/main/java/com/codeit/team5/mopl/user/dto/request/UserRegisterRequest.java
+++ src/main/java/com/codeit/team5/mopl/user/dto/request/UserRegisterRequest.java
@@ -14,7 +14,7 @@ public record UserRegisterRequest(
 
         @NotBlank(message = "비밀번호는 필수입니다.")
         @Pattern(
-                regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d)\\S{8,}$",
                 message = "비밀번호는 영문자와 숫자를 포함하여 8자 이상이어야 합니다."
         )
         String password
