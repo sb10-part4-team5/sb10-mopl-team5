db53d323c5b7db8dad7a386a4c33c570a8ce4be1
shong9124 <shifty393@gmail.com>
2026-07-02 16:13:22 +0900

    fix: 코드래빗 리뷰반영 - 회원가입시와 마찬가지로 비밀번호 정책을 동일시함

--- src/main/java/com/codeit/team5/mopl/user/dto/request/ChangePasswordRequest.java
+++ src/main/java/com/codeit/team5/mopl/user/dto/request/ChangePasswordRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Pattern;
 public record ChangePasswordRequest(
         @NotBlank
         @Pattern(
-                regexp = "^(?=.*[A-Za-z])(?=.*\\d).{8,}$",
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d)\\S{8,}$",
                 message = "비밀번호는 영문과 숫자를 포함하여 8자 이상이어야 합니다."
         )
         String password
