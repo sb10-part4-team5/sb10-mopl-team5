2e1c63b9548177762abe1c27e148b3b76e4aeb23
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-07-03 14:46:04 +0900

    fix: PlaylistRepository에서 불필요한 메서드 제거
    --- 본문 ---
    - PlaylistRepository에서 owner 정보를 함께 조회하는 findOwnerById 메서드를 제거했습니다.
    - 해당 메서드는 더 이상 사용되지 않아 코드의 불필요한 부분을 정리했습니다.

--- src/main/java/com/codeit/team5/mopl/playlist/repository/PlaylistRepository.java
+++ src/main/java/com/codeit/team5/mopl/playlist/repository/PlaylistRepository.java
@@ -1,8 +1,6 @@
 package com.codeit.team5.mopl.playlist.repository;
 
-import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,9 +8,6 @@ import com.codeit.team5.mopl.playlist.entity.Playlist;
 import com.codeit.team5.mopl.playlist.repository.query.PlaylistQueryRepository;
 
 public interface PlaylistRepository extends JpaRepository<Playlist, UUID>, PlaylistQueryRepository {
-
-    @EntityGraph(attributePaths = {"owner", "owner.profileImage"})
-    Optional<Playlist> findOwnerById(UUID id);
 
     boolean existsByIdAndOwnerEmail(UUID id, String email);
 
