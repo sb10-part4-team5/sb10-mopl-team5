4e4d3c676662272df23e1889057984c8dfc1e4e2
Seongkyeong Kim <gimmy6707@gmail.com>
2026-07-03 11:01:44 +0900

    Feat: 리뷰 엔티티 작성

---
+++ src/main/java/com/codeit/team5/mopl/review/entity/Review.java
@@ -0,0 +1,52 @@
+package com.codeit.team5.mopl.review.entity;
+
+import com.codeit.team5.mopl.global.entity.BaseEntity;
+import com.codeit.team5.mopl.global.entity.BaseUpdatableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "reviews",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"content_id", "author_id"}))
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review extends BaseUpdatableEntity {
+
+    @Column(name = "content_id", nullable = false, columnDefinition = "uuid")
+    private UUID contentId;
+
+    @Column(name = "author_id", nullable = false, columnDefinition = "uuid")
+    private UUID authorId;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String text;
+
+    @Column(nullable = false)
+    private Double rating;
+
+    public static Review create(UUID contentId, UUID authorId, String text, Double rating) {
+        return new Review(contentId, authorId, text, rating);
+    }
+
+    private Review(UUID contentId, UUID authorId, String text, Double rating) {
+        this.contentId = contentId;
+        this.authorId = authorId;
+        this.text = text;
+        this.rating = rating;
+    }
+
+    public void update(String text, Double rating) {
+        if (text != null) {
+            this.text = text;
+        }
+        if (rating != null) {
+            this.rating = rating;
+        }
+    }
+}
