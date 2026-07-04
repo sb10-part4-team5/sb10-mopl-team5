846e0ebda6715715a309580570630b1c90b42e20
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-07-04 20:04:37 +0900

    [Refactor] WatchingSessionService ISP 및 mapper, dto Fixes #218

---
+++ .init/init-db.sql
@@ -0,0 +1,5 @@
+CREATE USER discodeit_user WITH PASSWORD 'discodeit1234';
+CREATE DATABASE discodeit OWNER discodeit_user;
+
+-- CREATE USER mopl WITH PASSWORD 'mopl';
+-- CREATE DATABASE mopl OWNER mopl;
