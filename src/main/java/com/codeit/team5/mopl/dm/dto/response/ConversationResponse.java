05fbf57b2c7d33c1eba4f4a2f4c7d20dac81e9e9
dustle <pwdustle@gmail.com>
2026-07-04 16:35:55 +0900

    fix: 대화 목록 최신 메시지 JSON 키를 프론트에 맞춤 (lastestMessage)

--- src/main/java/com/codeit/team5/mopl/dm/dto/response/ConversationResponse.java
+++ src/main/java/com/codeit/team5/mopl/dm/dto/response/ConversationResponse.java
@@ -1,12 +1,13 @@
 package com.codeit.team5.mopl.dm.dto.response;
 
 import com.codeit.team5.mopl.user.dto.response.UserSummaryResponse;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.UUID;
 
 public record ConversationResponse(
         UUID id,
         UserSummaryResponse with,
-        DirectMessageResponse latestMessage,
+        @JsonProperty("lastestMessage") DirectMessageResponse latestMessage,
         boolean hasUnread
 ) {
 }
