0dd9ead3b83cde1909a95e8f391f4a1bf43dec7c
shong9124 <shifty393@gmail.com>
2026-07-05 13:19:13 +0900

    refactor: component 어노테이션 제거

--- src/main/java/com/codeit/team5/mopl/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/codeit/team5/mopl/auth/jwt/JwtAuthenticationFilter.java
@@ -11,10 +11,8 @@ import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
