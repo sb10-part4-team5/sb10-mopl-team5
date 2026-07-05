e9192cd5075207148c1efddccfae52b7007e7284
Seongkyeong Kim <gimmy6707@gmail.com>
2026-07-06 00:26:17 +0900

    Refactor: Playlist 코드 참조하여 parser 구현과 실패 시 예외 처리 구현

--- src/main/java/com/codeit/team5/mopl/review/contant/ReviewSortBy.java
+++ src/main/java/com/codeit/team5/mopl/review/contant/ReviewSortBy.java
@@ -1,23 +1,36 @@
 package com.codeit.team5.mopl.review.contant;
 
 import com.codeit.team5.mopl.review.exception.InvalidReviewSortByException;
-import java.util.Arrays;
+import com.codeit.team5.mopl.review.exception.ReviewSortByMismatchException;
+import java.time.Instant;
+import java.util.function.Function;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum ReviewSortBy {
-    CREATED_AT("createdAt"),
-    RATING("rating"),
+    CREATED_AT("createdAt", Instant::parse),
+    RATING("rating", Double::parseDouble),
     ;
 
-    private final String value;
+    private final String sortByType;
+    private final Function<String, ?> parser;
 
-    public static ReviewSortBy from(String value) {
-        return Arrays.stream(values())
-            .filter(sortBy -> sortBy.value.equalsIgnoreCase(value))
-            .findFirst()
-            .orElseThrow(() -> new InvalidReviewSortByException(value));
+    public static ReviewSortBy from(String string) {
+        for (ReviewSortBy value : ReviewSortBy.values()) {
+            if (string.equalsIgnoreCase(value.sortByType)) {
+                return value;
+            }
+        }
+        throw new InvalidReviewSortByException(string);
+    }
+
+    public Object parse(String cursor){
+        try{
+            return parser.apply(cursor);
+        } catch (Exception e){
+            throw new ReviewSortByMismatchException(sortByType, cursor);
+        }
     }
 }
