6eadb42c63617b93a7802b8b4a60e0fb1f4d907b
plzslp <gyutann1171@gmail.com>
2026-07-08 14:17:48 +0900

    fix: Content/WatchingSession 바인딩을 WebConfig 컨버터로 통일

--- src/main/java/com/codeit/team5/mopl/content/controller/ContentController.java
+++ src/main/java/com/codeit/team5/mopl/content/controller/ContentController.java
@@ -6,13 +6,10 @@ import com.codeit.team5.mopl.content.dto.request.ContentCreateRequest;
 import com.codeit.team5.mopl.content.dto.request.ContentCursorRequest;
 import com.codeit.team5.mopl.content.dto.request.ContentUpdateRequest;
 import com.codeit.team5.mopl.content.dto.response.ContentResponse;
-import com.codeit.team5.mopl.content.entity.ContentSortByType;
-import com.codeit.team5.mopl.content.entity.ContentType;
 import com.codeit.team5.mopl.content.facade.ContentFacade;
 import com.codeit.team5.mopl.content.service.ContentService;
 import com.codeit.team5.mopl.global.dto.CursorResponse;
 import jakarta.validation.Valid;
-import java.beans.PropertyEditorSupport;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,10 +17,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,22 +35,6 @@ public class ContentController implements ContentApi {
 
     private final ContentService contentService;
     private final ContentFacade contentFacade;
-
-    @InitBinder
-    public void initBinder(WebDataBinder binder) {
-        binder.registerCustomEditor(ContentType.class, new PropertyEditorSupport() {
-            @Override
-            public void setAsText(String text) {
-                setValue(ContentType.from(text));
-            }
-        });
-        binder.registerCustomEditor(ContentSortByType.class, new PropertyEditorSupport() {
-            @Override
-            public void setAsText(String text) {
-                setValue(ContentSortByType.from(text));
-            }
-        });
-    }
 
     @Override
     @PreAuthorize("hasRole('ADMIN')")
