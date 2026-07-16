e787d0d1662bce1877860a6d5adf43c0c63c6533
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-07-16 14:23:08 +0900

    refactor: Redis 직렬화 설정에 빌더 패턴 적용
    --- 본문 ---
    - RedisConfig 및 RedisRepository에서 Redis 직렬화기 인스턴스 생성 방식을 빌더 패턴으로 변경했습니다.
    - GenericJackson2JsonRedisSerializer.builder().objectMapper(mapper).build() 형태로 변경하여 코드의 가독성을 높이고 일관된 객체 생성 방식을 유지합니다.

--- src/main/java/com/codeit/team5/mopl/global/infra/redis/repository/RedisRepository.java
+++ src/main/java/com/codeit/team5/mopl/global/infra/redis/repository/RedisRepository.java
@@ -3,8 +3,10 @@ package com.codeit.team5.mopl.global.infra.redis.repository;
 import java.util.Optional;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import com.codeit.team5.mopl.global.infra.redis.config.RecordSupportingTypeResolver;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public abstract class RedisRepository<T> {
@@ -16,7 +18,11 @@ public abstract class RedisRepository<T> {
         this.redisTemplate.setConnectionFactory(connectionFactory);
         this.redisTemplate.setKeySerializer(new StringRedisSerializer());
 
-        Jackson2JsonRedisSerializer<T> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, clazz);
+        ObjectMapper mapper = RecordSupportingTypeResolver.createRedisObjectMapper(objectMapper);
+
+        RedisSerializer<Object> serializer = GenericJackson2JsonRedisSerializer.builder()
+                        .objectMapper(mapper)
+                        .build();
         this.redisTemplate.setValueSerializer(serializer);
         this.redisTemplate.afterPropertiesSet();
     }
