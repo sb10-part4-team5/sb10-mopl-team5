e5447884bef5b29ac0c08793d44758af8da63f2c
dustle <pwdustle@gmail.com>
2026-07-17 09:03:23 +0900

    refactor: 레디스 캐시 별 타입 명시 방식으로 수정

--- src/main/java/com/codeit/team5/mopl/global/infra/redis/config/RedisConfig.java
+++ src/main/java/com/codeit/team5/mopl/global/infra/redis/config/RedisConfig.java
@@ -1,24 +1,21 @@
 package com.codeit.team5.mopl.global.infra.redis.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Duration;
 import java.util.Map;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-@Configuration
-@EnableCaching
+// @Configuration
+// @EnableCaching
 public class RedisConfig {
 
-    @Bean
+    // @Bean
     public CacheManager cacheManager(RedisConnectionFactory factory, ObjectMapper objectMapper) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))  // 기본 TTL 10분
