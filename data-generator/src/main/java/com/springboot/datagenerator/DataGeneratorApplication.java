57026f76d5a3fa735eff230a9ba8b715510b9e1f
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-07-23 19:13:32 +0900

    fix: Elasticsearch 자동 설정 제외
    --- 본문 ---
    - Elasticsearch 관련 자동 설정을 제외하여 데이터 생성기 애플리케이션이 Elasticsearch에 의존하지 않도록 수정했습니다.
    - 이는 데이터 생성 기능 자체에 집중하고, Elasticsearch 연결 관련 문제를 방지하기 위함입니다.

--- data-generator/src/main/java/com/springboot/datagenerator/DataGeneratorApplication.java
+++ data-generator/src/main/java/com/springboot/datagenerator/DataGeneratorApplication.java
@@ -2,8 +2,19 @@ package com.springboot.datagenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.elasticsearch.ReactiveElasticsearchClientAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                ElasticsearchDataAutoConfiguration.class,
+                ElasticsearchClientAutoConfiguration.class,
+                ReactiveElasticsearchClientAutoConfiguration.class,
+                ElasticsearchRepositoriesAutoConfiguration.class
+        }
+)
 public class DataGeneratorApplication {
 
     public static void main(String[] args) {
