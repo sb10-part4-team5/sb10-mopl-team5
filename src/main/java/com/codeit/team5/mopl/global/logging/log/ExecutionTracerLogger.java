62578a9d207146d9189418304c3626fae2ebf768
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-07-23 19:12:48 +0900

    fix: 로깅 누락 문제 해결 및 로깅 상세화
    --- 본문 ---
    - ContentStatsEventListener에 @ExecutionTracer 어노테이션을 추가하여 해당 클래스의 모든 메서드에 대한 로깅이 활성화되도록 수정했습니다.
    - ExecutionTracerLogger에서 @Around 어노테이션의 로직을 수정하여, 메서드 레벨의 @ExecutionTracer 어노테이션이 없을 경우 클래스 레벨의 어노테이션을 찾도록 개선했습니다.
    - 로깅 레벨을 debug에서 info로 변경하여 더 중요한 실행 정보를 기록하도록 했습니다.
    - verbose 옵션이 true일 때만 인자를 로깅하도록 하여 불필요한 로그를 줄였습니다.

--- src/main/java/com/codeit/team5/mopl/global/logging/log/ExecutionTracerLogger.java
+++ src/main/java/com/codeit/team5/mopl/global/logging/log/ExecutionTracerLogger.java
@@ -3,6 +3,8 @@ package com.codeit.team5.mopl.global.logging.log;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 import lombok.extern.slf4j.Slf4j;
@@ -12,17 +14,23 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class ExecutionTracerLogger {
 
-    @Around("(@annotation(ExecutionTracer) || @within(ExecutionTracer))")
-    public Object logAround(ProceedingJoinPoint joinPoint, ExecutionTracer tracer)
-            throws Throwable {
+    @Around("@annotation(ExecutionTracer) || @within(ExecutionTracer)")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
         String fullClassName = joinPoint.getSignature().getDeclaringTypeName();
         String className = fullClassName.substring(fullClassName.lastIndexOf('.') + 1);
         String methodName = joinPoint.getSignature().getName();
         String where = String.join(".", className, methodName);
-        boolean verbose = tracer.verbose();
+
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        ExecutionTracer tracer = AnnotationUtils.findAnnotation(signature.getMethod(), ExecutionTracer.class);
+        if (tracer == null) {
+            tracer = AnnotationUtils.findAnnotation(joinPoint.getTarget().getClass(), ExecutionTracer.class);
+        }
+
+        boolean verbose = tracer != null && tracer.verbose();
         Object[] args = joinPoint.getArgs();
         String argsStr = verbose ? String.join(", ", formatArgs(args)) : "(omitted)";
-        log.debug("[start] {} | args: {}", where, argsStr);
+        log.info("[start] {} | args: {}", where, argsStr);
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         Object result;
@@ -32,7 +40,7 @@ public class ExecutionTracerLogger {
             stopWatch.stop();
         }
 
-        log.debug("[end] {} | time: {}ms | result: {}", where, stopWatch.getTotalTimeMillis(),
+        log.info("[end] {} | time: {}ms | result: {}", where, stopWatch.getTotalTimeMillis(),
                 result);
         return result;
     }
